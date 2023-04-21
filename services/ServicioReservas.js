fc770bdc7280c0f698c3586bb9bcb7707306291b
anamonsalve <anamonsalve96@gmail.com>
2023-04-21 18:17:29 -0500

    adelanto de tarea de servicio de reservas

---
+++ services/ServicioReservas.js
@@ -0,0 +1,22 @@
+import { modeloReserva } from "../models/modeloReserva.js";
+export class ServicioReservas{
+
+    constructor(){}
+
+    async registrarReserva(datosReserva){
+        let nuevareserva= new modeloReserv(datosReserva)
+        return await nuevareserva.save()
+    }
+
+    async buscarTodasReservas(){
+        let reservasConsultadas=await modeloReserva.find()
+        return reservasConsultadas
+    }
+
+    async buscarReserva(idReserva){
+        let reservasConsultada= await modeloReserva.findById(idReserva)
+        return reservasConsultada
+    }
+
+    
+}
